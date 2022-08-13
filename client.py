45a8568d2d729f552051c1919ec6f746242dbb2d
Tiago Henrique <tiagohrpg@gmail.com>
2022-08-12 22:25:06 -0300

    bug fix and comments

--- client.py
+++ client.py
@@ -1,23 +1,21 @@
 import os
 import socket
-import threading
 
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 1024 # tamanho do buffer
 
 SERVER_IP = "localhost" # endereco IP do servidor
 
-SERVER_PORT = 5000 # porta que o servidor esta esperando a msg
+SERVER_PORT = 5001 # porta que o servidor esta esperando a msg
 
 SERVER_DEST = (SERVER_IP, SERVER_PORT)
 
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# criando o socket udp
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # criando o socket udp
 
 msg  = ""
 
 files = []
 
-# Leitura de todos os jogos da pasta
+# Leitura de todos os arquivos da pasta
 directory = 'files'
 for i, filename in enumerate(os.listdir(directory)): 
     f = os.path.join(directory, filename) 
@@ -26,39 +24,25 @@ for i, filename in enumerate(os.listdir(directory)):
         print(filename)
         files.append(filename)
 
-os.chdir(directory)
+os.chdir(directory) 
 
 for file in files:
-    filename_encoded = bytes(file, "utf-8")
-    udp.sendto (filename_encoded, SERVER_DEST)
+    filename_encoded = bytes(file, "utf-8") # codificar o nome do arquivo
+    udp.sendto(filename_encoded, SERVER_DEST) # enviar o nome do arquivo para o servidor
     
     with open(file, "rb") as f:
         while True:
-            bytes_read = f.read(BUFFER_SIZE)
+            bytes_read = f.read(BUFFER_SIZE) # ler o arquivo
             if not bytes_read:
-                print("mandou um arquivo todo - cliente") 
-                udp.sendto (b"ENDFILE", SERVER_DEST)               
-                # file transmitting is done
+                print(f"arquivo '{file}' enviado") 
+                udp.sendto(b"ENDFILE", SERVER_DEST) # flag de fim do arquivo 
                 break
-
-            udp.sendto (bytes_read, SERVER_DEST)
+            else:
+                udp.sendto(bytes_read, SERVER_DEST) # enviar o arquivo em bytes enquanto tiver
 
         f.close()
 
-udp.sendto (b"END", SERVER_DEST)
+print("Todos os arquivos foram enviados")
+udp.sendto (b"END", SERVER_DEST) # flag de fim de todos os arquivos da pasta
 
-udp.close()
-
-# """
-# while(msg != "END"):
-
-#     SERVER_DEST = (SERVER_IP, SERVER_PORT)
-
-#     msg = input("Digite uma mensagem: ")
-
-#     udp.sendto (bytes(msg, "utf8"), SERVER_DEST)
-#     # enviar mensagem para o destino desejado
-#     # deve-se converter para bytes
-# """
-
-#udp.close()
+udp.close() # fechar o socket
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ BUFFER_SIZE = 1024
 
 SERVER_IP = '' # '' = significa que ouvira em todas as interfaces
 
-SERVER_PORT = 5000 # Porta que o servidor vai ouvir
+SERVER_PORT = 5001 # Porta que o servidor vai ouvir
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # criando o socket UDP
 
@@ -13,48 +13,38 @@ SERVER = (SERVER_IP, SERVER_PORT)
 
 msg_decoded = ""
 
-directory = "new_files"
+directory = "new_files" # diretório para guardar os arquivos
 
-udp.bind(SERVER)
-# faz o bind do ip da porta para comecar a ouvir
+udp.bind(SERVER) # faz o bind do ip da porta para comecar a ouvir
 
-os.mkdir(directory)
+try:
+    os.mkdir(directory) # cria o diretório e entra nele
+except: 
+    pass
+
 os.chdir(directory)
 
 while True:
-    filename_msg, client_address = udp.recvfrom(BUFFER_SIZE)
+    filename, client_address = udp.recvfrom(BUFFER_SIZE) # recebe o nome do arquivo
+    filename = filename.decode("utf-8")
 
-    if filename_msg == b"END":
-        print("acabei tudo - server")
+    if filename == "END": # verifica se os arquivos terminaram
+        print("Todos os arquivos foram recebidos")
         break
 
-    filename = f"new-{filename_msg.decode('utf-8')}"
+    new_filename = f"new-{filename}"
 
     bytes_read = b""
 
-    with open(filename, "wb") as f:
+    with open(new_filename, "wb") as f:
         while True:
-            bytes_read, _ = udp.recvfrom(BUFFER_SIZE)
-            print("------------file arrived-------------")
-            if bytes_read == b"ENDFILE":
-                print("recebi arquivo - server")
-                break # acabou o arquivo
+            bytes_read, client_address = udp.recvfrom(BUFFER_SIZE) # recebe os dados do arquivo
+            if bytes_read == b"ENDFILE": # verifica se o arquivo terminou
+                print(f"recebi o arquivo '{filename}'")
+                break
 
-            f.write(bytes_read)
+            f.write(bytes_read) # escreve os dados recebidos no arquivo criado
 
         f.close()
 
-udp.close()
-
-# while(msg_decoded != "\END"):
-
-#     msg_bytes, end_cliente = udp.recvfrom(1024)
-#     # o parametro indica o tamanho do buffer de recebimento (deve ser potencia de 2)
-#     # retorna uma string de dados e o endereco de de quem enviou
-
-#     msg_decoded = msg_bytes.decode("utf-8")
-
-
-#     print(f"Recebi {msg_decoded} do cliente {end_cliente}")
-
-# udp.close() # fim do socket
+udp.close() # fecha o socket
