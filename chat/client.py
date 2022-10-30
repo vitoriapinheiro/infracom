f2f2d15d78e8501367ab93d8d6b046a395c3136f
vitoriapinheiro <vps3@cin.ufpe.br>
2022-10-30 15:14:13 -0300

    final comments

--- chat/client.py
+++ chat/client.py
@@ -1,11 +1,10 @@
 import socket
 import threading
 import random
-# from colorama import Fore, Back, Style
 
-BUFFER_SIZE = 1024 # tamanho do buffer
+BUFFER_SIZE = 1024      # tamanho do buffer
 
-num_seq = b'0' # número de sequencia inicial
+num_seq = b'0'          # número de sequencia inicial
 
 expected_num_seq = b'1' # primeiro número de sequencia esperado
 
@@ -21,7 +20,7 @@ def send_ACK(num_seq, client_address):
 
     client.sendto(expected_num_seq, client_address) # envia o ACK com o número de sequencia esperado
     
-    if num_seq == expected_num_seq: # se o que foi recebido era o esperado, muda o número esperado
+    if num_seq == expected_num_seq:                 # se o que foi recebido era o esperado, muda o número esperado
         expected_num_seq =  b'0' if (expected_num_seq == b'1') else b'1'
 
 def send_pkt(msg):
@@ -30,26 +29,25 @@ def send_pkt(msg):
     num_seq = b'0' if (num_seq == b'1') else b'1'
 
     pkt = num_seq + msg
-    client.sendto(pkt, SERVER) # enviar o pacote em bytes enquanto tiver
+    client.sendto(pkt, SERVER)                     # enviar o pacote em bytes enquanto tiver
     correct_ACK = False
     while not correct_ACK:
         if socket.timeout:
             break
 
 
-name = input('Digite seu nome: ')
+name = input('Digite seu nome: ')                  # usuário coloca o nome para entrar no chat
 
+# thread para receber as mensagens
 def receive_message():
     global num_seq, expected_num_seq, correct_ACK
     while True:
         try:
             message, _ = client.recvfrom(BUFFER_SIZE)
-            if (message == b'0' or message == b'1'):# se for ACK
+            if (message == b'0' or message == b'1'):   # se for ACK
                 correct_ACK = True
             else:
-
                 print(message.decode())
-                # print(Style.RESET_ALL)
         except:
             pass
 
@@ -61,6 +59,7 @@ send_pkt(f'hi, meu nome eh:{name}'.encode())
 while True:
     message = input()
     send_pkt(f'{name}: {message}'.encode())
+    # se enviar um bye, termina a thread
     if message == 'bye':
         t.join()
         break
--- chat/server.py
+++ chat/server.py
@@ -12,7 +12,7 @@ num_seq_list = []     # número de sequencia inicial
 
 expected_num_seq = [] # primeiro número de sequencia esperado       
 
-timer = 30            # tempo em segundos para poder banir um usuário
+timer = 30            # tempo em 30 segundos para poder banir um usuário
 ban_timer = []        # ultima  vez que um usuario baniu
 
 clients_names = []    # nome dos usuários conectados
@@ -27,34 +27,26 @@ server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('localhost', 9999))
 
 
-
+# envia ACK correto
 def send_ACK(num_seq, client_address):
     global expected_num_seq
     index = clients.index(client_address)
 
     server.sendto(expected_num_seq[index], client_address) # envia o ACK com o número de sequencia esperado
     
-    if num_seq == expected_num_seq[index]: # se o que foi recebido era o esperado, muda o número esperado
+    if num_seq == expected_num_seq[index]:                 # se o que foi recebido era o esperado, muda o número esperado
         expected_num_seq[index] =  b'0' if (expected_num_seq[index] == b'1') else b'1'
 
-def correct_ACK(index):
-    global num_seq
-
-    ACK_msg, _ = server.recvfrom(1) # espera receber o ACK
-
-    if (ACK_msg == num_seq[index]):
-        return True
-    else:
-        return False
-
+# envia pacote para o destino
 def send_pkt(msg, address, index):
     global num_seq_list
 
     num_seq_list[index] = b'0' if (num_seq_list[index] == b'1') else b'1'
 
     pkt = msg
-    server.sendto(pkt, address) # enviar o pacote em bytes enquanto tiver
+    server.sendto(pkt, address)                          # enviar o pacote em bytes enquanto tiver
 
+# thread que recebe as mensagens e coloca em uma fila
 def receive_message():
     while True:
         try:
@@ -102,7 +94,7 @@ def broadcast():
 
         # mensagem de verificação de ban
         if (message.decode().split(':')[1].strip()).startswith('ban @'):
-          if time.time() >= (ban_timer[index_sender] + timer): # Verifica se o usuário que está banindo não está na lista de banidos
+          if time.time() >= (ban_timer[index_sender] + timer):          # Verifica se o usuário que está banindo não está na lista de banidos
             in_ban_name = (message.decode().split(':')[1].strip()).split('@')[1]
 
             for i in range(len(clients)):
@@ -123,46 +115,55 @@ def broadcast():
           if message.decode().startswith('hi, meu nome eh:'):
             name = message.decode().split(':')[1]
             
-            # se o cliente estiver na lista de banimentos e recebe um aviso
+            # se o cliente estiver na lista de banimentos e tenta enviar uma msg, os outros recebem um aviso
             if (name in banned_names):
               send_pkt(f'[{local_time}] O usuario {name} esta banido, nao pode entrar'.encode(), client, i)
               if clients[i] == address:       # se o usuario que tentou entrar estive banido
                 clients.pop(i)
                 num_seq_list.pop(i)
                 expected_num_seq.pop(i)
+
             # se nao tiver, adiciona o cliente nas listas e envia a mensagem aos usuarios
             else:
-              if i == 0:                      # only append once
+              if i == 0:
                 clients_names.append(name)
                 ban_counter.append([])
               send_pkt(f'[{local_time}]hi, meu nome eh:{name}'.encode(), client, i)
           else:
             message_content = message.decode().split(':')[1]
             name_request = message.decode().split(':')[0]
+            # se o nome de quem enviou a mensagem estiver na lista de banimento, ele recebe um aviso
             if name_request in banned_names:
               send_pkt(f'[{local_time}] Você esta banido, nao pode enviar mensagens'.encode(), address, 1)
               break
-
+            
             else:
+              # se a mensagem for um bye
               if message_content == " bye":
                 send_pkt(f'[{local_time}] {name_request} saiu do chat! :('.encode(), client, i)
 
+              # se o a mensagem for um comando de listagem
               elif message_content == " list":
                 if name_request == clients_names[i]:
                   send_pkt(f'[{local_time}] {clients_names}'.encode(), client, i)
-
+              
+              # se a mensagem for uma mensagem privada, envia apenas para o destinatário
               elif (message_content.strip()).startswith(f'@{clients_names[i]}'):
                 send_pkt(f'[{local_time}] {name_request}:{message_content}'.encode(), client, i)
 
+              # envia mensagem se um voto for adicionado
               if vote_checker:
                 send_pkt(f'[{local_time}] {len(ban_counter[in_ban_index])}/{len(clients_names)} - ban {in_ban_name}'.encode(), client, i)
-                
+              
+              # envia mensagem se um jogador for banido
               if ban_checker:
                 send_pkt(f'[{local_time}] O usuario {clients_names[in_ban_index]} foi banido!!!'.encode(), client, i)
-
+              
+              # se a mensagem nao for privada, envia para todos
               if not (message_content.strip()).startswith(f'@'):
                 send_pkt(f'[{local_time}] {message.decode()}'.encode(), client, i)
 
+        # retira o usuário da lista caso ele seja banido ou saia do chat
         if len(ban_counter[banned_index]) >= 2*len(clients_names)/3:
           banned_names.append(clients_names[banned_index])
           clients.pop(banned_index)
@@ -172,6 +173,8 @@ def broadcast():
           expected_num_seq.pop(banned_index)
           ban_timer.pop(banned_index)
           banned_index = 0
+
+        # retira o usuário da lista caso ele saia do chat
         if message.decode().split(':')[1] == " bye":
           index = clients_names.index(name_request)
           clients.pop(index)
@@ -180,9 +183,6 @@ def broadcast():
           num_seq_list.pop(index)
           expected_num_seq.pop(index)
           ban_timer.pop(index)
-
-          
-
 
 t1 = threading.Thread(target=receive_message)
 t2 = threading.Thread(target=broadcast)
